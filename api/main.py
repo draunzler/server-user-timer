67c0984f63f72e14fc9294d10f4169ad1d5c79ff
draunzler <shadymeister2017@gmail.com>
2024-05-20 03:24:36 +0530

    changed the CORS url from localhost to vercel

--- api/main.py
+++ api/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://server-user-timer.vercel.app/"],
     allow_credentials=True,
     allow_methods=["GET"],
     allow_headers=["*"],
