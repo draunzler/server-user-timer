0d8b934622be1448df66833c5e837f48bfd9dea7
draunzler <shadymeister2017@gmail.com>
2024-05-20 03:33:39 +0530

    made necessary changes for render deployment

--- api/main.py
+++ api/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from datetime import datetime, timezone
 from fastapi.middleware.cors import CORSMiddleware
 import pytz
+import os
 
 app = FastAPI()
 
@@ -19,3 +20,7 @@ async def get_server_time():
     server_time = datetime.now(jst)
     server_time = server_time.strftime("%m/%d/%Y, %I:%M:%S %p")
     return {"serverTime": server_time}
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
