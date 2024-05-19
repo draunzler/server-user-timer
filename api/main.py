349c63e329b0fda4b08b3d7f761fde265546dd55
draunzler <shadymeister2017@gmail.com>
2024-05-20 03:38:09 +0530

    fixed CORS error

--- api/main.py
+++ api/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://server-user-timer.vercel.app/"],
+    allow_origins=["https://server-user-timer.vercel.app"],
     allow_credentials=True,
     allow_methods=["GET"],
     allow_headers=["*"],
